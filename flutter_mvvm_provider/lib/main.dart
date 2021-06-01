456676f2a17d4a238c94cc8b553b06dc7fd66fb3
zakkyna <zakkynurali@gmail.com>
2021-06-01 23:34:27 +0700

    Update main.dart

--- flutter_mvvm_provider/lib/main.dart
+++ flutter_mvvm_provider/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'Toko Gadai Owner',
+        title: 'Flutter MVVM Provider',
         navigatorKey: locator<NavigationService>().navigatorKey,
         theme: ThemeData(
           visualDensity: VisualDensity.adaptivePlatformDensity,
